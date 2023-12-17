8e4ec7f75e3d42a67c4bd62dd01377554c3c84d3
moonflower <tayler.brianne.baldwin@gmail.com>
2023-12-17 14:30:17 -0500

    Changes to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -10,23 +10,24 @@ $(document).ready(function () {
   localStorage.setItem(time, text);
 })
 
-    //Local storage- representing each time slot. When text is entered the "save" button stores the data. 
-    //When you refresh page, the data is still secured in time slots.
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+//Local storage- representing each time slot. When text is entered the "save" button stores the data. 
+//When you refresh page, the data is still secured in time slots.
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
 
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
 
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
 
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
 
-    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
 
-    $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
 
-    $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
 
-    $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
 
-    $("#hour-5 .description").val(localStorage.getItem("hour-5"));
-  });
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+  
+});
